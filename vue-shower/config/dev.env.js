c2c953a35d0c544c76089d2879985f8f1edd35b4
kuibobo <kuibobo@gmail.com>
2020-08-01 17:07:21 +0800

    修改前端配置

--- vue-shower/config/dev.env.js
+++ vue-shower/config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API:'"http://localhost:8080/spider"'
+  BASE_API:'"http://127.0.0.1:8080/spider"'
   // BASE_API:'"http://192.168.2.157:8080/service"'
 })
